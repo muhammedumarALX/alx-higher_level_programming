2ea5831fcb17e67100c6df45bedfe22be1ac2b96
muhammed umar <muhammedumaralx@gmail.com>
2022-12-20 10:15:46 +0100

    Task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-#!/urs/bin/python3
-def safe_print_list(my_list=[], x=0):
-	count = 0
-
-	for i in range(x):
-		try:
-			print("{}".format(my_list[i]), end=' ')
-		except:
-			break
-		else:
-			count += 1
-
-	print()
-	return (count)
-=======
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
@@ -21,11 +5,10 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end=' ')
-        except:
+        except Exception:
             break
         else:
             count += 1
 
         print()
         return (count)
->>>>>>> e4667039a0aaa14f75ad4b30a61e5e8dfceaf68a
