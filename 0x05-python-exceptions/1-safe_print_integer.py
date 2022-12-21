5043416c13740c5265150388be48aecbf7b6eb13
muhammed umar <muhammedumaralx@gmail.com>
2022-12-22 00:59:36 +0100

    project 5

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return (False)
-    else (True)
+        return True
+    except (ValueError, TypeError):
+        return False
