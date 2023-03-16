a925117e88306d1af58d12ca5ee597ee780df46c
muhammed umar <muhammedumaralx@gmail.com>
2023-03-16 01:29:24 +0100

    Task 7

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArr = ['C', 'Python is cool', 'JavaScript is amazing'];
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myArr.length; i++) {
   console.log(myArr[i]);
 }
