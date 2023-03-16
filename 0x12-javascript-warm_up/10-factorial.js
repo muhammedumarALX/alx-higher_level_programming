2bdcdafeaad8d9ade3790795fec2ff28b02bf9a1
muhammed umar <muhammedumaralx@gmail.com>
2023-03-16 20:54:54 +0100

    Task 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (num) {
-  let total = 1;
-  for (let i = num; i > 1; i--) {
-    total *= i;
+  if ((Number.isNaN(num)) || (num === 1)) {
+    return 1;
+  } else {
+    return factorial(num - 1) * num;
   }
-  return total;
 }
 
 console.log(factorial(parseInt(process.argv[2])));
