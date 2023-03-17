733408d7ed9b6ca40740efc277c91b89d36be46a
muhammed umar <muhammedumaralx@gmail.com>
2023-03-17 18:53:05 +0100

    Task 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 let biggest = 0;
 const arrayNumbers = [];
+let i;
 
-for (let i = 2; i < process.argv.length; i++) {
-    if (Number.isNaN(parseInt(process.argv[i])) === false) {
+for (i = 2; i < process.argv.length; i++) {
+  if (Number.isNaN(parseInt(process.argv[i])) === false) {
     arrayNumbers[i - 2] = parseInt(process.argv[i]);
-    }
+  }
 }
 
 if (arrayNumbers.length > 1) {
-    biggest = Math.max.apply(null, arrayNumbers);
-    i = arrayNumbers.indexOf(biggest);
-    arrayNumbers[i] = -Infinity;
-    biggest = Math.max.apply(null, arrayNumbers);
+  biggest = Math.max.apply(null, arrayNumbers);
+  i = arrayNumbers.indexOf(biggest);
+  arrayNumbers[i] = -Infinity;
+  biggest = Math.max.apply(null, arrayNumbers);
 }
 
 console.log(biggest);
