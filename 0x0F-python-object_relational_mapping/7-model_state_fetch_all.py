1655868f6370622fc89e1342356d8340d4787136
muhammed umar <muhammedumaralx@gmail.com>
2023-03-22 16:03:11 +0100

    Task 8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,4 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 """script that lists all State objects from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
@@ -7,7 +7,7 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = engine_create("mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     # Create a;ll the table associated with the Base metadata
