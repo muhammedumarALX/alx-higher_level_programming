28e37b67ff37a53321d22378bfc753ac58c29ee5
muhammed umar <muhammedumaralx@gmail.com>
2023-03-23 11:37:51 +0100

    Task 14

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,10 +17,8 @@ if __name__ == "__main__":
     session = Session()
 
     # Query all Stat objects from the database and order by states.id
-    state_delete = session.query(State).filter(State.name.like("%a%")).all()
-
-    # delete
-    session.delete(state_delete)
+    state_delete = session.query(State).filter(State.name.like("%a%"))\
+        .delete(synchronize_session='fetch')
 
     # commit changes
     session.commit()
