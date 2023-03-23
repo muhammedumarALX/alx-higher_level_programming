fbd4612291fa261e85ff992473eca2782c923ecb
muhammed umar <muhammedumaralx@gmail.com>
2023-03-23 11:16:05 +0100

    Task 13

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # Create a;ll the table associated with the Base metadata
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all Stat objects from the database and order by states.id
+    state = session.query(State).filter_by(id=2).first()
+
+    # udate the atrribute
+    state.name = "New Mexico"
+
+    # commit changes
+    session.commit()
+
+    # close the session
+    session.close()
