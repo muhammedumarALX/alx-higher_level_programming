32f1eb0f06313705d33a10f923e0d9b14b221774
muhammed umar <muhammedumaralx@gmail.com>
2023-03-23 11:02:54 +0100

    Task 12

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # Create a;ll the table associated with the Base metadata
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all Stat objects from the database and order by states.id
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+
+    session.commit()
+
+    # print the State objects in the format specified
+    print("{}".format(new_state.id))
+    session.close()
