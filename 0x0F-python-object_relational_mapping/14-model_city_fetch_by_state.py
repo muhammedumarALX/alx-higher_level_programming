7ddf734d7f4223035e23dd285fa31cdb2a92c716
muhammed umar <muhammedumaralx@gmail.com>
2023-03-24 00:12:51 +0100

    task 15

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,8 @@ from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     # Create a;ll the table associated with the Base metadata
     Base.metadata.create_all(engine)
@@ -18,11 +19,12 @@ if __name__ == "__main__":
     session = Session()
 
     # Query all Stat objects from the database
-    cities = session.query(City).join(State).order_by(City.id).all()
+    query = session.query(City, State)
+    states = query.filter(City.state_id == State.id).order_by(City.id)
 
     # Print cties
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    for city, state in states:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     # close the session
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+""" Defines a City class
+    Inherits from SQLAlchemy Base and links to the MySQL table cities
+"""
+
 from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 from model_state import Base
 
 
 class City(Base):
+     """ Represents a city column for a MySQL table
+         __tablename__ (str): The name of the MySQL table to store Cities.
+        id (sqlalchemy.Integer): The city's id.
+        name (sqlalchemy.String): The city's name.
+        state_id(sqlalchemy.Integer): Foreign key to states.id
+    """
     __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
