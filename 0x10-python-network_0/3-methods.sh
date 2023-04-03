658acde32a2e0881d5a4efaf9c6eb29314c99e1e
muhammed umar <muhammedumaralx@gmail.com>
2023-04-03 18:33:00 +0100

    Task 4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that take in a URL and displays all HTTP methods
-curl -sI "$1" | grep "Allow:" | awk '{print $2,$3,$4,$5,$6,$7}'
+curl -sI "$1" | grep "Allow:" | cut -d ' ' -f2-
