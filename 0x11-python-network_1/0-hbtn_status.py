1b1f0ec064d461f9badecd004a093fcfc8e23002
muhammed umar <muhammedumaralx@gmail.com>
2023-04-07 00:04:57 +0100

    Task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     address = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(address) as response:
         html = response.read()
-    print("Body response: ")
+    print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))
