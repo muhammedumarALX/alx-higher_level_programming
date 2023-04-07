1ee079f716d9dabc10087f1e2338de96a6f72b13
muhammed umar <muhammedumaralx@gmail.com>
2023-04-07 15:32:36 +0100

    Task 4

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that sends a url and prints error message"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {}".format(e.code))
