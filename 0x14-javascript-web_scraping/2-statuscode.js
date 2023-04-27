2da510f1c95e0670c27f6b08b86744a37c238e26
muhammed umar <muhammedumaralx@gmail.com>
2023-04-27 17:15:36 +0100

    Task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,10 +6,8 @@ const request = require('request');
 // gets the first argument from the command line
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log("code: ${response.statusCode}");
+request(url, (err, res) => {
+  if (err == null) {
+     console.log(`code: ${response.statuscode}`);
   }
 });
